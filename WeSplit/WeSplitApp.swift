2fc6e746817a08e85edea7aa7a711ca899ea8e25
AdriGoM <adriangodoymartinez@icloud.com>
2023-01-22 20:17:02 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adri on 22/1/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
